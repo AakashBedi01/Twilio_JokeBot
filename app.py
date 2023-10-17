f51e7061a17f6dcb46722992076b2cf7a99cfa91
Aakash Bedi <akashdeepsinghbedi@gmail.com>
2023-10-17 14:18:51 -0300

    added message response capabilities

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
+from typing import Any
+
 import requests
 from flask import Flask, request
 from twilio.twiml.voice_response import VoiceResponse
+from twilio.twiml.message_response import MessagingResponse
 
 
 def get_joke():
@@ -17,6 +20,7 @@ def get_joke():
 
 app = Flask(__name__)
 
+
 @app.route('/voice', methods=['POST'])
 def voice():
     joke = get_joke()
@@ -24,6 +28,15 @@ def voice():
     resp.say(joke)
 
     return str(resp)
+
+
+@app.route('/sms', methods=['POST'])
+def sms():
+    print(request.form)
+    joke: get_joke()
+    number = request.form['From']
+    resp = MessagingResponse()
+    resp.message(f"hello {number}, This is your joke of the day! \n {joke} ")
 
 
 if __name__ == "__main__":
