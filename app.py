57da502f4b5c8692c3efcbb06c523c993d9af99a
Aakash Bedi <akashdeepsinghbedi@gmail.com>
2023-10-17 16:07:03 -0300

    added error handling

--- app.py
+++ app.py
@@ -1,43 +1,49 @@
-# Import necessary modules
 from flask import Flask, request
 from twilio.twiml.voice_response import VoiceResponse
 from twilio.twiml.messaging_response import MessagingResponse
 import requests
 
-# Function to retrieve a random joke from a web API
+
 def get_joke():
     url = "https://v2.jokeapi.dev/joke/Programming,Miscellaneous?blacklistFlags=religious,political,racist,sexist"
-    response = requests.get(url)
-    if response.status_code == 200:
+    try:
+        response = requests.get(url)
+        response.raise_for_status()  # Check for HTTP request errors
         joke_data = response.json()
         if joke_data.get("type") == "single":
             return joke_data.get("joke")
         elif joke_data.get("type") == "twopart":
             return f"{joke_data.get('setup')} {joke_data.get('delivery')}"
+    except requests.exceptions.RequestException as e:
+        return f"Sorry, I couldn't retrieve a joke. Error: {str(e)}"
+    except Exception as e:
+        return f"An error occurred: {str(e)}"
     return "Sorry, I couldn't find a joke for you."
 
-# Initialize a Flask web application
+
 app = Flask(__name__)
 
-# Route for handling voice requests
+
 @app.route('/voice', methods=['POST'])
 def voice():
     joke = get_joke()
     resp = VoiceResponse()
-    resp.say(joke)  # Respond with the retrieved joke using voice
+    resp.say(joke)
 
     return str(resp)
 
-# Route for handling SMS (text) requests
+
 @app.route('/sms', methods=['POST'])
 def sms():
-    joke = get_joke()
-    number = request.form['From']
-    resp = MessagingResponse()
-    resp.message(f"Hello {number}, this is your joke of the day!\n{joke}")  # Respond with the retrieved joke via SMS
+    try:
+        joke = get_joke()
+        number = request.form['From']
+        resp = MessagingResponse()
+        resp.message(f"Hello {number}, this is your joke of the day!\n{joke}")
+        return str(resp)
+    except Exception as e:
+        return f"An error occurred: {str(e)}"
 
-    return str(resp)
 
-# Start the Flask application when this script is run
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
