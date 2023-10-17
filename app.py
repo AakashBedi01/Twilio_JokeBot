beb12322a9f34aa2757dab8a25f193b0da45771e
Aakash Bedi <akashdeepsinghbedi@gmail.com>
2023-10-17 15:29:09 -0300

    final changes

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
+# Import necessary modules
 from flask import Flask, request
 from twilio.twiml.voice_response import VoiceResponse
 from twilio.twiml.messaging_response import MessagingResponse
 import requests
 
+# Function to retrieve a random joke from a web API
 def get_joke():
     url = "https://v2.jokeapi.dev/joke/Programming,Miscellaneous?blacklistFlags=religious,political,racist,sexist"
     response = requests.get(url)
@@ -14,24 +16,28 @@ def get_joke():
             return f"{joke_data.get('setup')} {joke_data.get('delivery')}"
     return "Sorry, I couldn't find a joke for you."
 
+# Initialize a Flask web application
 app = Flask(__name__)
 
+# Route for handling voice requests
 @app.route('/voice', methods=['POST'])
 def voice():
     joke = get_joke()
     resp = VoiceResponse()
-    resp.say(joke)
+    resp.say(joke)  # Respond with the retrieved joke using voice
 
     return str(resp)
 
+# Route for handling SMS (text) requests
 @app.route('/sms', methods=['POST'])
 def sms():
-    joke = get_joke()  # Fix the typo here
+    joke = get_joke()
     number = request.form['From']
     resp = MessagingResponse()
-    resp.message(f"Hello {number}, this is your joke of the day!\n{joke}")
+    resp.message(f"Hello {number}, this is your joke of the day!\n{joke}")  # Respond with the retrieved joke via SMS
 
     return str(resp)
 
+# Start the Flask application when this script is run
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
